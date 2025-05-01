8a3592ea29d305e985017bbaacaf0ac1c008825d
ssmith2006 <shantel.smith@hotmail.com>
2025-05-01 03:27:27 +0000

    deleted starter code in index,jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  const API_KEY=process.env.API_KEY;
+  return (
+    <>
+    <div>App</div>
+    </>
+    
+  )
+}
+console.log(process.env.API_KEY);
